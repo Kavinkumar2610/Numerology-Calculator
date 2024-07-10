ba3556e4d98e6b8ffc1ae106e6b6eea34b103eb9
kavin26 <kavinkumar@juristech.net>
2024-07-10 16:01:11 +0800

    East Plate

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-var letterValues = { 'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 
-                     'F': 8, 'G': 3, 'H': 5, 'I': 1, 'J': 1,
-                     'K': 2, 'L': 3, 'M': 4, 'N': 5, 'O': 7,
-                     'P': 8, 'Q': 1, 'R': 2, 'S': 3, 'T': 4, 
-                     'U': 6, 'V': 6, 'W': 6, 'X': 5, 'Y': 1,
-                     'Z': 7 };
+var letterValues = {
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5,
+    'F': 8, 'G': 3, 'H': 5, 'I': 1, 'J': 1,
+    'K': 2, 'L': 3, 'M': 4, 'N': 5, 'O': 7,
+    'P': 8, 'Q': 1, 'R': 2, 'S': 3, 'T': 4,
+    'U': 6, 'V': 6, 'W': 6, 'X': 5, 'Y': 1,
+    'Z': 7
+};
 
-
-document.getElementById("plateForm").addEventListener("submit", function(event) {
+document.getElementById("plateForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     var letters = document.getElementById("letters").value.toUpperCase();
     var digitSum = parseInt(document.getElementById("digitSum").value);
+    var endingSeries = document.getElementById("endingSeries").value.toUpperCase();
     var desiredTotals = [];
-    
+
     var desiredTotal1 = document.getElementById("desiredTotal1").value.trim();
     if (desiredTotal1 !== "") {
         desiredTotals.push(parseInt(desiredTotal1));
     }
-    
+
     var desiredTotal2 = document.getElementById("desiredTotal2").value.trim();
     if (desiredTotal2 !== "") {
         desiredTotals.push(parseInt(desiredTotal2));
     }
-    
+
     var desiredTotal3 = document.getElementById("desiredTotal3").value.trim();
     if (desiredTotal3 !== "") {
         desiredTotals.push(parseInt(desiredTotal3));
     }
 
-    var validPlateNumbers = generatePlateNumbers(letters, digitSum, desiredTotals);
+    var validPlateNumbers = generatePlateNumbers(letters, digitSum, endingSeries, desiredTotals);
 
     var resultDiv = document.getElementById("result");
     resultDiv.innerHTML = "";
@@ -37,58 +39,59 @@ document.getElementById("plateForm").addEventListener("submit", function(event) 
         document.getElementById("showOtherNumbersBtn").style.display = "block";
     } else {
         var table = "<h2>Results</h2><table><tr><th>Plate Series</th><th>Plate Number</th><th>Desired Total</th></tr>";
-        validPlateNumbers.forEach(function(plate) {
+        validPlateNumbers.forEach(function (plate) {
             table += "<tr><td>" + plate[0] + "</td><td>" + plate[1].substring(letters.length) + "</td><td>" + plate[2] + "</td></tr>";
         });
         table += "</table>";
         resultDiv.innerHTML = table;
         document.getElementById("showOtherNumbersBtn").style.display = "block";
     }
-    
+
     var resultHeader = document.querySelector("#result h2");
     var infoIcon = document.createElement("i");
-        infoIcon.classList.add("fas", "fa-info-circle", "info-icon");
-        infoIcon.style.marginLeft = "10px";
-        infoIcon.style.color = "#74C0FC";
-        infoIcon.addEventListener("click", function() {
+    infoIcon.classList.add("fas", "fa-info-circle", "info-icon");
+    infoIcon.style.marginLeft = "10px";
+    infoIcon.style.color = "#74C0FC";
+    infoIcon.addEventListener("click", function () {
         var modal = document.getElementById("myModal");
         modal.style.display = "block";
 
         var span = document.getElementsByClassName("close")[0];
-        span.onclick = function() {
+        span.onclick = function () {
             modal.style.display = "none";
         }
 
-        window.onclick = function(event) {
+        window.onclick = function (event) {
             if (event.target == modal) {
-            modal.style.display = "none";
+                modal.style.display = "none";
             }
         }
-        });
-        resultHeader.appendChild(infoIcon);
+    });
+    resultHeader.appendChild(infoIcon);
 
-    var letterSum = Array.from(letters).reduce(function(sum, char) {
+    var letterSum = Array.from(letters + endingSeries).reduce(function (sum, char) {
         return sum + (letterValues[char] || 0);
     }, 0);
     document.getElementById("letterSum").innerHTML = "<p>Total Plate Series Value: " + letterSum + "</p>";
     // document.getElementById("plateSum").innerHTML = "<p>Total Plate Number Value: " + digitSum + "</p>";
 });
 
-document.getElementById("showOtherNumbersBtn").addEventListener("click", function() {
+document.getElementById("showOtherNumbersBtn").addEventListener("click", function () {
     var letters = document.getElementById("letters").value.toUpperCase();
     var digitSum = parseInt(document.getElementById("digitSum").value);
+    var endingSeries = document.getElementById("endingSeries").value.toUpperCase();
     var desiredTotals = [];
-    
+
     var desiredTotal1 = document.getElementById("desiredTotal1").value.trim();
     if (desiredTotal1 !== "") {
         desiredTotals.push(parseInt(desiredTotal1));
     }
-    
+
     var desiredTotal2 = document.getElementById("desiredTotal2").value.trim();
     if (desiredTotal2 !== "") {
         desiredTotals.push(parseInt(desiredTotal2));
     }
-    
+
     var desiredTotal3 = document.getElementById("desiredTotal3").value.trim();
     if (desiredTotal3 !== "") {
         desiredTotals.push(parseInt(desiredTotal3));
@@ -100,14 +103,14 @@ document.getElementById("showOtherNumbersBtn").addEventListener("click", functio
     var otherNumbersResultDiv = document.getElementById("otherNumbersResult");
     otherNumbersResultDiv.innerHTML = "";
 
-    var otherNumbers = generateOtherNumbers(letters, digitSum, desiredTotals, letterValues, limit);
+    var otherNumbers = generateOtherNumbers(letters, digitSum, endingSeries, desiredTotals, letterValues, limit);
 
     if (otherNumbers.length === 0) {
         otherNumbersResultDiv.innerHTML = "<p>No other plate numbers found.</p>";
     } else {
         var table = "<table id='otherNumbersTable'><tr><th>Plate Number</th><th>Desired Total</th></tr>";
-        otherNumbers.forEach(function(number) {
-            table += "<tr><td>" + letters  + " " + number[0]  + "</td><td>" + number[1] + "</td></tr>";
+        otherNumbers.forEach(function (number) {
+            table += "<tr><td>" + letters + " " + number[0] + " " + endingSeries + "</td><td>" + number[1] + "</td></tr>";
         });
         table += "</table>";
         otherNumbersResultDiv.innerHTML = table;
@@ -115,7 +118,7 @@ document.getElementById("showOtherNumbersBtn").addEventListener("click", functio
     populateFilterDropdown(desiredTotals);
 });
 
-document.getElementById("searchInput").addEventListener("input", function() {
+document.getElementById("searchInput").addEventListener("input", function () {
     var searchValue = this.value.toLowerCase();
     var rows = document.getElementById("otherNumbersTable").getElementsByTagName("tr");
 
@@ -138,13 +141,13 @@ document.getElementById("searchInput").addEventListener("input", function() {
 function populateFilterDropdown(desiredTotals) {
     var filterDropdown = document.getElementById("filterDropdown");
     filterDropdown.innerHTML = "<option value='all'>Show All</option>";
-    desiredTotals.forEach(function(total) {
+    desiredTotals.forEach(function (total) {
         filterDropdown.innerHTML += "<option value='" + total + "'>" + total + "</option>";
     });
 }
 
 
-document.getElementById("filterDropdown").addEventListener("change", function() {
+document.getElementById("filterDropdown").addEventListener("change", function () {
     var filterValue = this.value;
     var rows = document.getElementById("otherNumbersTable").getElementsByTagName("tr");
 
@@ -164,11 +167,11 @@ document.getElementById("filterDropdown").addEventListener("change", function() 
     }
 });
 
-document.getElementById("limitDropdown").addEventListener("change", function() {
+document.getElementById("limitDropdown").addEventListener("change", function () {
     document.getElementById("showOtherNumbersBtn").click();
 });
 
-document.querySelector(".clear-btn").addEventListener("click", function(event) {
+document.querySelector(".clear-btn").addEventListener("click", function (event) {
     event.preventDefault();
     document.getElementById("plateForm").reset();
     document.getElementById("result").innerHTML = "";
@@ -185,29 +188,29 @@ function calculateSingleDigitSum(number) {
     return number;
 }
 
-function generatePlateNumbers(letters, digitSum, desiredTotals) {
+function generatePlateNumbers(letters, digitSum, endingSeries, desiredTotals) {
     var validPlateNumbers = [];
-    var letterSum = Array.from(letters).reduce(function(sum, char) {
+    var letterSum = Array.from(letters + endingSeries).reduce(function (sum, char) {
         return sum + (letterValues[char] || 0);
     }, 0);
-    
-    desiredTotals.forEach(function(desiredTotal) {
+
+    desiredTotals.forEach(function (desiredTotal) {
         var totalSum = letterSum + digitSum;
         var singleDigitSum = calculateSingleDigitSum(totalSum);
         if (singleDigitSum === desiredTotal) {
-            validPlateNumbers.push([letters, letters + digitSum, desiredTotal]);
+            validPlateNumbers.push([letters, letters + digitSum + endingSeries, desiredTotal]);
         }
     });
 
     return validPlateNumbers;
 }
 
-function generateOtherNumbers(letters, digitSum, desiredTotals, letterValues, limit) {
+function generateOtherNumbers(letters, digitSum, endingSeries, desiredTotals, letterValues, limit) {
     var otherNumbers = [];
     var maxLimit = (limit === "all") ? 9999 : parseInt(limit);
-    
+
     for (var i = 1; i <= 9999; i++) {
-        var letterSum = Array.from(letters).reduce(function(sum, char) {
+        var letterSum = Array.from(letters + endingSeries).reduce(function (sum, char) {
             return sum + (letterValues[char] || 0);
         }, 0);
         var totalSum = letterSum + i + digitSum;
@@ -223,12 +226,12 @@ function generateOtherNumbers(letters, digitSum, desiredTotals, letterValues, li
     return otherNumbers;
 }
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
     var desiredTotals = [];
     var desiredTotal1 = document.getElementById("desiredTotal1").value.trim();
     var desiredTotal2 = document.getElementById("desiredTotal2").value.trim();
     var desiredTotal3 = document.getElementById("desiredTotal3").value.trim();
-    
+
     if (desiredTotal1 !== "") {
         desiredTotals.push(parseInt(desiredTotal1));
     }
@@ -242,7 +245,7 @@ window.addEventListener("DOMContentLoaded", function() {
     populateFilterDropdown(desiredTotals);
 });
 
-document.getElementById("infoIcon").addEventListener("click", function() {
+document.getElementById("infoIcon").addEventListener("click", function () {
     var infoWindow = window.open("https://names4brands.com/images/indian-numerology-alphabet-chart-download.jpg", "_blank", "width=600,height=400");
     infoWindow.focus();
 });
